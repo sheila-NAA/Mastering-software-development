39f9aa55e9a8e03ef38a011d45d85f584fefd8e1
sheila-NAA <59737033+sheila-NAA@users.noreply.github.com>
2021-04-06 15:50:15 +0000

    Update geom_timelines.R

--- R/geom_timelines.R
+++ R/geom_timelines.R
@@ -1,149 +1,110 @@
-#' Time line of earthquakes
+#' Timeline of Earthquakes
 #'
-#' \code{geom_timeline} return a \code{\link[ggplot2]{layer}}
-#'     representing a time line of earthquakes, i.e. a time line over
-#'     which there are a point for each earthquake.
+#' \code{geom_timeline} shows a timeline of NOAA Significant earthquakes,
+#' plotting individual countries along the y-axis and dates along the x-axis.
+#' The size of the points is relatative to the earthquakes' magnitude, and
+#' the color is related to the total number of deaths.
 #'
-#' @details Aesthetics:
-#' \code{geom_timeline} understands the following aesthetics
-#'   (required in bold):
+#' It is highly recommended that this geom is not used in isolation. For
+#' best results, use it with the \code{\link{eq_timeline}} wrapper function.
+#'
+#' @param mapping See \code{ggplot2} \code{\link[ggplot2]{layer}}
+#' @param data See \code{ggplot2} \code{\link[ggplot2]{layer}}
+#' @param stat See \code{ggplot2} \code{\link[ggplot2]{layer}}
+#' @param position See \code{ggplot2} \code{\link[ggplot2]{layer}}
+#' @param na.rm See \code{ggplot2} \code{\link[ggplot2]{layer}}
+#' @param show.legend See \code{ggplot2} \code{\link[ggplot2]{layer}}
+#' @param inherit.aes See \code{ggplot2} \code{\link[ggplot2]{layer}}
+#' @param ... other arguments passed on to \code{\link{layer}}.
+#'
+#' @section Aesthetics:
+#' \code{geom_timeline} undertands the following aesthetics (required
+#' aesthetics are in bold):
 #' \itemize{
-#'   \item \strong{x}: (Date) of earthquakes
-#'   \item y: (factr) stratification. If present multiple time lines
-#'            will be plotted for each level of the factor
-#'            (e.g. country).
-#'   \item colour: of the points
-#'   \item size: of the points
-#'   \item alpha: trasparency for the points
-#'   \item fill
-#'   \item linetype
-#'   \item linesize
-#'   \item fontsize
-#'   \item stroke
+#'  \item \strong{x}: recommend \code{DATE}
+#'  \item y: recommend \code{COUNTRY}
+#'  \item size: recommend \code{EQ_PRIMARY}
+#'  \item color: recommend \code{TOTAL_DEATHS}
+#'  \item alpha
+#'  \item shape
+#'  \item stroke
+#'  \item fill
 #' }
-#'
-#' @inheritParams ggplot2::layer
-#' @param na.rm (lgl, default = FALSE) remove missing data?
-#' @param ... further arguments passed to the geom layer
-#'
-#' @importFrom ggplot2 layer
-#'
 #' @export
+#'
 #' @examples
-#' \dontrun{
-#'     library(dplyr)
-#'     library(lubridate)
-#'     library(ggplot2)
+#' library(dplyr); library(ggplot2)
+#' quakes <- eq_load_clean_data()
 #'
-#'     library(devrcap)
-#'         data(noaa)
-#'
-#'     noaa %>%
-#'         eq_clean_data() %>%
-#'         filter(
-#'             country %in% c("ITALY", "GREECE", "PORTUGAL"),
-#'             year(date) >= 1900
-#'         ) %>%
-#'         ggplot(aes(
-#'             x = date,
-#'             y = country,
-#'             size   = eq_primary,
-#'             colour = log(total_deaths)
-#'         )) +
-#'         geom_timeline()
-#' }
-geom_timeline <- function(
-  mapping = NULL, data = NULL, stat = "identity",
-  position = "identity", show.legend = NA, inherit.aes = TRUE,
-  ..., na.rm = FALSE
-) {
-  ggplot2::layer(
-    geom = GeomTimeline, mapping = mapping, data = data,
-    stat = stat, position = position, show.legend = show.legend,
-    inherit.aes = inherit.aes, params = list(na.rm = na.rm, ...)
-  )
+#' quakes %>%
+#'   dplyr::filter(COUNTRY %in% c('USA', 'UK')) %>%
+#'   dplyr::filter(DATE > '2000-01-01') %>%
+#'   ggplot() +
+#'   geom_timeline(aes(x = DATE, y = COUNTRY, color = TOTAL_DEATHS,
+#'                     size = EQ_PRIMARY)) +
+#'   scale_size_continuous(name = 'Richter scale value') +
+#'   scale_color_continuous(name = '# of Deaths') +
+#'   theme_eq()
+geom_timeline <- function(mapping = NULL,
+                          data = NULL,
+                          stat = "identity",
+                          position = "identity",
+                          na.rm = FALSE,
+                          show.legend = NA,
+                          inherit.aes = TRUE,
+                          ...) {
+    ggplot2::layer(
+      geom = GeomTimeline,
+      mapping = mapping,
+      data = data,
+      stat = stat,
+      position = position,
+      show.legend = show.legend,
+      inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    )
 }
 
-
-#' @rdname geom_timeline
+#' GeomTimeline
+#'
+#' See \code{\link{geom_timeline}} for description.
+#'
 #' @format NULL
 #' @usage NULL
-#'
-#' @importFrom ggplot2 ggproto Geom aes draw_key_point
-#' @importFrom grid pointsGrob unit gpar polylineGrob gList
-#' @importFrom scales alpha
-#'
 #' @export
 GeomTimeline <- ggplot2::ggproto("GeomTimeline", ggplot2::Geom,
+  required_aes = c('x'),
 
-                                 required_aes = c("x"),
+  default_aes = ggplot2::aes(
+    y = 0,
+    size = 1,
+    color = 'grey50',
+    alpha = 0.5,
+    shape = 19,
+    stroke = 0.5,
+    fill = NA
+  ),
 
-                                 default_aes = ggplot2::aes(
+  draw_key = ggplot2::draw_key_point,
 
-                                   y        = 0.25,
-                                   colour   = "grey",
-                                   size     = 1,
-                                   alpha    = 0.25,
-                                   shape    = 19,
-                                   fill     = "black",
-                                   linesize = 0.5,
-                                   linetype = 1 ,
-                                   fontsize = 10,
-                                   stroke   = 1
-                                 ),
+  draw_panel = function(data, panel_scales, coord) {
 
-                                 draw_key = ggplot2::draw_key_point,
+    coords <- coord$transform(data, panel_scales)
 
-                                 setup_data = function(data, params) {
+    # resize the coordinates' size to more reasonable values
+    coords$size <-
+      coords$size / max(coords$size) * 1.5
 
-                                   if ("colour" %in% colnames(data)) {
-                                     warning(paste(
-                                       "missing values for colour.",
-                                       "They were replaced with the minimum value."
-                                     ))
-                                     data$colour[is.na(data$colour)] <-
-                                       min(data$colour, na.rm = TRUE)
-                                   }
-                                   data
-                                 },
-
-                                 draw_panel = function(data, panel_scales, coord) {
-                                   coords <- data %>%
-                                     coord$transform(panel_scales)
-
-                                   if (length(unique(coords$y)) == 1) {
-                                     coords$y <- 0.25
-                                   }
-
-                                   points <- grid::pointsGrob(
-                                     x    = coords$x,
-                                     y    = coords$y,
-                                     size = grid::unit(coords$size / 5, "char"),
-                                     pch  = coords$shape,
-                                     gp   = grid::gpar(
-                                       col      = coords$colour %>%
-                                         scales::alpha(coords$alpha),
-                                       fill     = coords$fill %>%
-                                         scales::alpha(coords$alpha),
-                                       fontsize = grid::unit(coords$fontsize, "points")
-                                     )
-                                   )
-
-                                   y_lines <- unique(coords$y)
-
-                                   lines <- grid::polylineGrob(
-                                     x  = grid::unit(
-                                       rep(c(0, 1), each = length(y_lines)),
-                                       "npc"
-                                     ),
-                                     y  = grid::unit(c(y_lines, y_lines), "npc"),
-                                     id = rep(seq_along(y_lines), 2),
-                                     gp = grid::gpar(
-                                       col = "grey",
-                                       lwd = grid::unit(coords$linesize[1], "mm")
-                                     )
-                                   )
-
-                                   grid::gList(points, lines)
-                                 }
+    ## Construct a grid grob
+    grid::pointsGrob(
+      x = coords$x,
+      y = coords$y,
+      pch = coords$shape,
+      gp = grid::gpar(
+        col = coords$colour,
+        alpha = coords$alpha,
+        cex = coords$size
+      )
+    )
+  }
 )
