5584d416e7c4fb71066f32c09d7475a3672bdc43
sheila-NAA <59737033+sheila-NAA@users.noreply.github.com>
2021-04-06 15:45:16 +0000

    Update eq_clean_data.R

--- R/eq_clean_data.R
+++ R/eq_clean_data.R
@@ -1,71 +1,168 @@
-#'Package: buildingpackage
-#'Title: Cleaning raw data
-#'Date: 06/04/2021
-#'Version: 0.0.0.9000
-#'Authors@R:
-#'  person(given = "Sheila",
-#'         family = "Amoafo",email = "amoafosheila@outloo.com",
-#'         role = c("aut", "cre"))
-#'Description: This package cleans raw a dataframe into understandable
-#'  and neat dataframe.
-#'License: `use_mit_license()`
-#'Encoding: UTF-8
-#'LazyData: true
-#'Roxygen: list(markdown = TRUE)
-#'RoxygenNote: 7.1.1
+#' Clean Earthquake Data
 #'
+#' \code{eq_clean_data} takes a data frame of NOAA earthquake data and cleans it
+#' up for further analysis.
 #'
-#' @details
-#' To asses the request we have to produce a [data frame][data.frame]
-#' which has:
+#' This function takes a data frame of NOAA earthquake data, which may be
+#' obtained from NOAA's
+#' \href{https://www.ngdc.noaa.gov/nndc/struts/form?t=101650&s=1&d=1}{Significant
+#' EarthQuake Database}, or loaded into the envirnoment with \code{data(quakes)}
+#' (April 1, 2017 snapshot), and cleans it up for further analysis.  In
+#' particular, it creates a \code{DATE} feature by combing the \code{YEAR},
+#' \code{MONTH}, and \code{DAY} features, and makes sure the \code{LATITUDE} and
+#' \code{LONGITUDE} features are of type \code{numeric}.  Also converts the
+#' \code{DEATHS}, \code{TOTAL_DEATHS}, and \code{EQ_PRIMARY} (Richter scale
+#' value) to type \code{numeric}.
 #'
-#' - A date column created by uniting the year, month, day and
-#'   converting it to the Date class
-#' - latitude and longitude columns converted to numeric class
-#' - using \code{\link{eq_location_clean}} cleans the location_name
-#'   column by stripping out the country name (including the colon) and
-#'   converts names to title case (as opposed to all caps).
+#' For the \code{DATE} variable, note that R does not handle years before 1CE
+#' correctly (see documentation for \code{\link[base]{as.Date}}). This function
+#' has attempted to make an approximation of those dates, but the user should
+#' handle with care.  Also, for some events the month and day are not available,
+#' so those are approximated by January and 01, respectively.
 #'
-#' @note For simplicity of coding and consistency, we reduced all the
-#'       column names to lowercase too.
+#' @param data A data frame of NOAA significant earthquake data, similar to what
+#'   can be loaded with \code{data(quakes)}. At a minimum, the data needs to
+#'   have \code{YEAR}, \code{MONTH}, \code{DAY}, \code{LATITUDE},
+#'   \code{LONGITUDE}, \code{DEATHS}, \code{TOTAL_DEATHS}, and \code{EQ_PRIMARY}
+#'   features.
 #'
-#' @param data A raw data frame of NOAA data
+#' @return A \code{tbl_df} with the same supplied data, but with
+#'   \code{LATITUDE}, \code{LONGITUDE}, \code{DEATHS}, \code{TOTAL_DEATHS}, and
+#'   \code{EQ_PRIMARY} converted from \code{character} to \code{numeric}, and a
+#'   new column \code{DATE}.
 #'
-#' @return a [tibble][tibble::tibble-package] (which is also a
-#'         [data frame][data.frame]).
 #' @export
 #'
 #' @examples
-#' data(noaa)
-#' eq_clean_data(noaa)
+#'
+#' data(quakes)
+#' quakes_clean <- eq_clean_data(quakes)
 eq_clean_data <- function(data) {
- names(data) <- tolower(names(data))
 
+  required_vars <- c('LONGITUDE', 'LATITUDE', 'MONTH', 'DAY', 'YEAR',
+                     'DEATHS', 'TOTAL_DEATHS', 'EQ_PRIMARY')
+  purrr::map(required_vars, function(rv) {
+    if(!(rv %in% names(data))) {
+      stop('Missing required variable: ', rv)
+    }
+  })
 
-    data %>%
-    dplyr::mutate(
-          location_name = eq_location_clean(location_name)
-              ) %>%
-    dplyr::filter(!is.na(year)) %>%
-    dplyr::mutate(year = as.character(year)) %>%
-    dplyr::mutate_at(
-        dplyr::vars(month:second),
-        dplyr::funs(
-          purrr::map_chr(.,
-             ~ dplyr::if_else(is.na(.x),
-                 "01",
-                   stringr::str_pad(.x, width = 2, pad = "0")
-               )
-            )
-        )
-    ) %>%
-    tidyr::unite(col = "date", year, month, day,
-       sep    = "-",
-       remove = FALSE
-    )%>%
-    dplyr::mutate(
-      date = good_date(year, month, day)
-    )
+  data <- data %>%
+    dplyr::mutate_(
+      LONGITUDE = ~as.numeric(LONGITUDE),
+      LATITUDE = ~as.numeric(LATITUDE),
+      MONTH = ~ifelse(is.na(MONTH), 1, MONTH),
+      DAY = ~ifelse(is.na(DAY), 1, DAY),
+      DEATHS = ~as.numeric(DEATHS),
+      TOTAL_DEATHS = ~as.numeric(TOTAL_DEATHS),
+      EQ_PRIMARY = ~as.numeric(EQ_PRIMARY))
+
+  # as.Date doesn't handle BCE dates, so have to estimate it as best as possible
+  data <- data %>%
+    dplyr::mutate_(DATE = ~purrr::pmap(list(YEAR, MONTH, DAY),
+                                      function(y, m, d) {
+      if (y < 0) {
+        # numeric date for first DAY of CE
+        first_date <-
+          as.numeric(as.Date('0 1 1', '%Y %m %d', origin = '1970-01-01'))
+        # take the negative date and "mirror" it to CE Jan 1st
+        mirror_date <-
+          as.Date(paste(y * -1 - 1, 1, 1, sep = '-'), '%Y-%m-%d',
+                  origin = '1970-01-01')
+        dt <- as.numeric(mirror_date) - first_date
+
+        # time to end of year
+        end_of_year_dt <-
+          as.numeric(as.Date(paste(y * -1 + 2, 12, 31, sep = '-'), '%Y-%m-%d',
+                             origin = '1970-01-01')) -
+          as.numeric(as.Date(paste(y * -1 + 2, m, d, sep = '-'), '%Y-%m-%d',
+                             origin = '1970-01-01'))
+        date <-
+          as.Date(first_date - dt - end_of_year_dt + 1, origin = '1970-01-01')
+      } else {
+        date <- as.Date(paste(y, m, d, sep = '-'), '%Y-%m-%d')
+      }
+      date
+    })) %>%
+    dplyr::mutate_(DATE = ~unlist(DATE), # date comes out of purrr::map as numeric
+                  DATE = ~as.Date(DATE, origin = '1970-01-01'))
+
+  data
 }
 
-use_vignette("eq_clean_data")
+#' Clean Earthquake Location Field
+#'
+#' \code{eq_location_clean} takes a data frame of NOAA earthquake data and cleans
+#' the \code{LOCATION_NAME} field.
+#'
+#' This function cleans the \code{LOCATION_NAME} observation in a NOAA
+#' Significant Earthquakes data set.  It removes the country name (of form
+#' \code{COUNTRY:}) from the \code{LOCATION_NAME} data (unless the country is
+#' the only name present in that field), and converts the remainder of the field
+#' to Title Case. This will make the location name easier to read when plotting
+#' and mapping.
+#'
+#' @param data A data frame of NOAA significant earthquake data, similar to what
+#'   can be loaded with \code{data(quakes)}. At a minimum, the data needs to
+#'   have \code{LOCATION_NAME} and \code{COUNTRY} features to use this function.
+#'
+#' @return A \code{tbl_df} with the same supplied data, but with the
+#'   \code{LOCATION_NAME} variable cleaned up to remove the country name that is
+#'   supplied in the \code{COUNTRY} variable (unless the country name is the
+#'   \strong{only} word in the \code{LOCATION_NAME} observation), and convert
+#'   the remainder of the words in \code{LOCATION_NAME} to Title Case.
+#'
+#' @export
+#'
+#' @examples
+#' data(quakes)
+#' quakes_loc_clean <- eq_location_clean(quakes)
+eq_location_clean <- function(data) {
+
+  required_vars <- c('COUNTRY', 'LOCATION_NAME')
+  purrr::map(required_vars, function(rv) {
+    if(!(rv %in% names(data))) {
+      stop('Missing required variable: ', rv)
+    }
+  })
+
+  data <- data %>%
+    dplyr::mutate_(
+      LOCATION_NAME =
+        ~purrr::map2_chr(COUNTRY, LOCATION_NAME,
+                        function(COUNTRY, LOCATION_NAME) {
+                          gsub(paste0(COUNTRY, ":"), '', LOCATION_NAME)
+                        }),
+      LOCATION_NAME = ~stringr::str_trim(LOCATION_NAME),
+      LOCATION_NAME = ~stringr::str_to_title(LOCATION_NAME)
+    )
+
+  data
+}
+
+#' Load Clean Earthquakes Data
+#'
+#' \code{eq_load_clean_data} is a shortcut to loading and cleaning the NOAA
+#' \code{\link{quakes}} data supplied with this package.
+#'
+#' This function is a quick shortcut to load the \code{\link{quakes}} data
+#' supplied with this package.  If the April 1, 2017 cutoff is sufficient for
+#' you, use this function to save a few keystrokes.
+#'
+#' @return A \code{tbl_df} of NOAA Significant Earthquakes Data, cleaned with
+#'   the functions \link{eq_clean_data} and \link{eq_location_clean}.
+#'
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#' quakes1 <- eq_load_clean_data()
+#' # is equivalent to:
+#' quakes2 <- quakes %>% eq_clean_data() %>% eq_location_clean()
+eq_load_clean_data <- function() {
+  eq <- get('quakes')
+  df <- eq %>%
+    eq_clean_data() %>%
+    eq_location_clean()
+  df
+}
